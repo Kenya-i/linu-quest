8005d7a050768547489474e5c3792a83d3883bbf
kenya <ken8116@icloud.com>
2026-07-24 03:44:35 +0900

    Add Postgres connection via pgx and containerize backend

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 func main() {
+
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
+		connString = "postgres://linuquest_user:password@localhost:5432/linuquest"
+	}
+
+	db, err := pgxpool.New(context.Background(), connString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(context.Background()); err != nil {
+		log.Fatal(err)
+	}
 
 	r := gin.Default()
 	r.Run(":8080")
